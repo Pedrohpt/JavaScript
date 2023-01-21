55ad77fcff1d5e73c78b8734d1f6cc4d211bb51c
pedohpt <pedro1112hpt@gmail.com>
2023-01-20 21:38:14 -0300

    Atividade 12 Update

--- Pedro-Atividade12/Atividade12-1/Atividade12-1.js
+++ Pedro-Atividade12/Atividade12-1/Atividade12-1.js
@@ -1,9 +1,16 @@
-var nota1 = parseInt(window.prompt("Digite a 1º nota"));
-var nota2 = parseInt(window.prompt("Digite a 2º nota"));
-var nota3 = parseInt(window.prompt("Digite a 3º nota"));
-var nota4 = parseInt(window.prompt("Digite a 4º nota"));
-var nota5 = parseInt(window.prompt("Digite a 5º nota"));
+nota = [0,0,0,0,0]
 
-result = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+soma = 0
 
-document.write("A média das notas é:" + result)
+for (var i = 0; i < 5; i++) {
+    nota[i] = parseFloat(window.prompt("Digite a uma nota"));
+}
+
+
+for(var i = 0; i < nota.length; i++) {
+    soma += nota[i];
+}
+
+media = (soma / 5)
+
+window.alert("A média das notas é:" + media)
